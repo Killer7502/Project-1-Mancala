1393d2eee7ba03ce4c408bd2c800005416b7b10f
Natalie Williams <willin51@ferris.edu>
2020-06-22 13:16:38 -0400

    Added basic outline for the GUI

--- Mancala/src/main/MancalaGUI.java
+++ Mancala/src/main/MancalaGUI.java
@@ -3,10 +3,19 @@ package main;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JSpinner;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.JTextArea;
+import javax.swing.JScrollPane;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.border.EtchedBorder;
 
 public class MancalaGUI {
 
-	private JFrame frame;
+	private JFrame frmMancala;
 
 	/**
 	 * Launch the application.
@@ -16,7 +25,7 @@ public class MancalaGUI {
 			public void run() {
 				try {
 					MancalaGUI window = new MancalaGUI();
-					window.frame.setVisible(true);
+					window.frmMancala.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -35,9 +44,37 @@ public class MancalaGUI {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmMancala = new JFrame();
+		frmMancala.setTitle("Mancala");
+		frmMancala.setBounds(100, 100, 1049, 632);
+		frmMancala.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+		GroupLayout groupLayout = new GroupLayout(frmMancala.getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+						.addComponent(scrollPane, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 1015, Short.MAX_VALUE)
+						.addComponent(panel, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 1015, Short.MAX_VALUE))
+					.addContainerGap())
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.TRAILING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(panel, GroupLayout.DEFAULT_SIZE, 351, Short.MAX_VALUE)
+					.addGap(59)
+					.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 165, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap())
+		);
+		
+		JTextArea textArea = new JTextArea();
+		scrollPane.setViewportView(textArea);
+		frmMancala.getContentPane().setLayout(groupLayout);
 	}
-
 }
