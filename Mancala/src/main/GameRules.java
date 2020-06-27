b4cf44767f86fbf16042557639fad59d8619ab79
Zachary Johnson <pendragos@gmail.com>
2020-06-27 18:43:21 -0400

    Added mancala skipping rules, empty pocket check and bonus turn check

--- Mancala/src/main/GameRules.java
+++ Mancala/src/main/GameRules.java
@@ -1,21 +1,68 @@
 package main;
 
 public class GameRules {
-	
-	void stoneMovement() {
+	boolean emptyPocket = false;
+	void stoneMovement(int [] Pocket, int selection, int playerTurn) {
 		int stoneNumber = Pocket[selection];//gets the current selected number of stones
 		Pocket[selection] = 0; // makes sure that that the selected pocket is emptied
 		selection += 1; //Makes sure that it starts adding to next pocket
-		for (x= = stoneNumber, x > 0, x--) {//Loops through until all stones are used 
-			if (selection == maxNumber) { //uses this if statment if max number of array is reached
-				Pocket[selection] +=1; // adds 1 to current pocket
-				selection = 0; // resets to begining 
+		for (int x = stoneNumber; x > 0; x--) {//Loops through until all stones are used 
+			if (selection == 14) { //uses this if statement if max number of array is reached
+				if (playerTurn == 2) {
+					Pocket[selection] +=1; // adds 1 to current pocket
+					if (x != 1) {
+						selection = 1; // resets to beginning 
+					}
+					
+				}
+				else {
+					selection = 1; // resets to beginning 
+					Pocket[selection] +=1; // adds 1 to current pocket
+				}
+				
 			}
+			
+			
 			else { // if still with in bounds of array
-				Pocket[selection] +=1;
-				selection += 1; // continues to next pocket
+				if (playerTurn == 2 && selection == 7) {
+					selection = 8; // continues to next pocket skipping opponents mancala 
+					Pocket[selection] +=1;
+					if (x != 1) {
+						selection += 1; // continues to next pocket
+					}
+					
+				}
+				else {
+					Pocket[selection] +=1;
+					if (x != 1) {
+						selection += 1; // continues to next pocket
+					}
+					
+				}
+				
+			}
+			
+			
+			//checks if on final stone if the pocket was empty and what players turn it was
+			if (Pocket[selection] == 1 && x == 1 && playerTurn == 1 && selection < 7) {
+				emptyPocket = true;
+			}
+			else if (Pocket[selection] == 1 && x == 1 && playerTurn == 2 && selection < 14 && selection > 7) {
+				emptyPocket = true;
+			}
+			
+			
+			if (Pocket[selection] == 1 && x == 1 && playerTurn == 1 && selection == 7) {
+				bonusTurn = true;
+			}
+			else if (Pocket[selection] == 1 && x == 1 && playerTurn == 2 && selection == 14) {
+				bonusTurn = true;
 			}
 		}
+		
+		
 	}
+	
+	
 	
 }
